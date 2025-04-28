80dc501ba89282e012055abd806e58df6b6a8466
Cristian Vega <cristianvega73212828@gmail.com>
2025-04-28 13:33:52 -0500

    Deshabilitando Pagos en credito desde POS y Venta Rapida

--- app/Http/Controllers/Tenant/PosController.php
+++ app/Http/Controllers/Tenant/PosController.php
@@ -191,7 +191,8 @@ class PosController extends Controller
             ->where([['establishment_id', auth()->user()->establishment_id], ['contingency', false]])
             ->get();
 
-        $payment_method_types = PaymentMethodType::all();
+        $payment_method_types = PaymentMethodType::NotCredit()->
+                where('id', '!=', '07')->get();
         $cards_brand = CardBrand::all();
         $payment_destinations = $this->getPaymentDestinations();
         $global_discount_types = ChargeDiscountType::whereIn('id', ['02', '03'])->whereActive()->get();
